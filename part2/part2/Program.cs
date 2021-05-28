ddc46efa040a183878603531d57c09cccc3c3103
guz86 <guzmo6@rambler.ru>
2021-05-29 01:29:18 +0300

    18+

--- part2/part2/Program.cs
+++ part2/part2/Program.cs
@@ -10,6 +10,20 @@ namespace part2
     {
         static void Main(string[] args)
         {
+            int age;
+            Console.Write("Сколько вам лет? ");
+            age = Convert.ToInt32(Console.ReadLine());
+            if (age>=18)
+            {
+                Console.WriteLine("Добро пожаловать в клуб!");
+                Console.WriteLine("Что вам налить?");
+            }
+            else
+            {
+                Console.WriteLine("Извините, мы вас не можем обслужить! Вы еще юны. ");
+                Console.WriteLine("Приходите к нам через "+ (18 - age) + " лет! Всего хорошего!");
+            }
+           
         }
     }
 }
