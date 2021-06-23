73e7a06ece441296ec0a5734ad4a7c4ab0f96f85
guz86 <guzmo6@rambler.ru>
2021-06-23 09:28:05 +0300

    console_ver1

--- part2/part2/Program.cs
+++ part2/part2/Program.cs
@@ -892,7 +892,7 @@ namespace part2
 
 
             // Конвертер валют Currency_Converter_ver3 через switch case и валютные пары
-
+            /*
             float userRUB;
             float userUSD;
             float userEUR;
@@ -1058,6 +1058,91 @@ namespace part2
                 }
 
             }
+            */
+
+
+
+            // Консольное меню
+            // При помощи всего, что вы изучили, создать приложение, которое может обрабатывать команды.
+            // Т.е.вы создаете меню, ожидаете ввода нужной команды, после чего выполняете действие, которое присвоено этой команде.
+            // Примеры команд(требуется 4 - 6 команд, придумать самим):
+            // SetName – установить имя;
+            // ChangeConsoleColor - изменить цвет консоли;
+            // SetPassword – установить пароль;
+            // WriteName – вывести имя(после ввода пароля);
+            // Esc – выход из программы.
+            // Программа не должна завершаться после ввода, пользователь сам должен выйти из программы при помощи команды.
+
+
+            // console_ver1
+            int inputMenu;
+            string name = "";
+            string password = "";
+
+            while (true)
+            {
+                Console.WriteLine(" | SetName (1) | ChangeConsoleColor (2) | | SetPassword (3) | WriteName (4) | Exit (10)");
+                inputMenu = Convert.ToInt32(Console.ReadLine());
+
+
+                switch (inputMenu)
+                {
+                    case 1:
+                        Console.Write("Введите имя: ");
+                        name = Console.ReadLine();
+                        Console.WriteLine("Имя задано, нажмите любую клавишу..");
+                        Console.ReadKey();
+                        Console.Clear();
+                        break;
+                    case 2:
+                        Console.BackgroundColor = ConsoleColor.DarkBlue;
+                        Console.WriteLine("Цвет фона изменен, нажмите любую клавишу..");
+                        Console.ReadKey();
+                        Console.Clear();
+                        break;
+                    case 3:
+                        Console.Write("Установите пароль: ");
+                        password = Console.ReadLine();
+                        Console.WriteLine("Пароль задан, нажмите любую клавишу..");
+                        Console.ReadKey();
+                        Console.Clear();
+                        break;
+                    case 4:
+                        Console.Write("Введите пароль: ");
+                        if (Console.ReadLine() == password)
+                        {
+                            Console.WriteLine("Добро пожаловать, "+ name +" Нажмите любую клавишу");
+                            Console.ReadKey();
+                        }
+                        Console.Clear();
+                        break;
+                    case 10:
+                        break;
+                    default:
+                        Console.WriteLine("Не верная команда");
+                        Console.Clear();
+                        break;
+                }
+                if (inputMenu == 10)
+                {
+                    break;
+                }
+
+
+
+
+
+
+            }
+
+
+
+
+
+
+
+
+
 
         }
 
