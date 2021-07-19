8fe3057fe633f0c1b5d201f937ca0ed183689111
guz86 <guzmo6@rambler.ru>
2021-07-20 00:03:40 +0300

    Random

--- part2/oneDimensionalArray/Program.cs
+++ part2/oneDimensionalArray/Program.cs
@@ -10,11 +10,15 @@ namespace oneDimensionalArray
             int[] arrayTwo = new int[3];
             string[] arrayThree = new string[3] { "dsf", "fsdf", "asdasd"};
 
+            Random rand = new Random();
 
             for (int i = 0; i < arrayTwo.Length; i++)
             {
+                arrayTwo[i] = rand.Next(0, 100);
                 Console.WriteLine(arrayTwo[i]);
             }
+
+            
 
 
         }
