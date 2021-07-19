f78bf8466a6bb373cde3d4b49d7a0b2bead31976
guz86 <guzmo6@rambler.ru>
2021-07-19 23:53:18 +0300

    init oneDimensionalArray

---
+++ part2/oneDimensionalArray/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace oneDimensionalArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
