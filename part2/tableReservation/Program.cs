ae8438d759164bac5758d430b44b0c31c27c2d47
guz86 <guzmo6@rambler.ru>
2021-07-21 20:26:07 +0300

    openCafe

--- part2/tableReservation/Program.cs
+++ part2/tableReservation/Program.cs
@@ -9,20 +9,59 @@ namespace tableReservation
             // свободные места для бронирования
             int[] tables = { 2, 4, 6, 5, 2, 3, 4, 2 };
             bool openCafe = true;
+
+
+
             while (openCafe)
             {
-                Console.Write("1 - выберите столик, 2 - выход: ");
+                Console.WriteLine("1 - выберите столик, 2 - выход: ");
+
+                Console.SetCursorPosition(0, 10);
+                for (int i = 0; i < tables.Length; i++)
+                {
+                    Console.WriteLine("Мест за столом " + (i + 1) + " : " + tables[i]);
+                }
+
+
+                Console.SetCursorPosition(0, 1); 
 
                 switch (Convert.ToInt32(Console.ReadLine()))
                 {
                     case 1:
+                        int userTable, UserPlace;
+
+                        Console.Write("Какой стол вы хотите выбрать: ");
+                        userTable = Convert.ToInt32(Console.ReadLine())-1;
+
+                        if ( userTable >= tables.Length || userTable < 0)
+                        {
+                            Console.WriteLine("Нет такого стола");
+                            break;
+                        }
+
+                        Console.Write("Сколько мест нужно: ");
+                        UserPlace = Convert.ToInt32(Console.ReadLine());
+                     
+
+                        if (tables[userTable] < UserPlace || UserPlace < 0)
+                        {
+                            Console.WriteLine("За этим столом нет "+ UserPlace + " мест");
+                            break;
+                        }
+
+                        tables[userTable] -= UserPlace;
 
                         break;
+
+
                     case 2:
                         openCafe = false;
                         break;
 
                 }
+                Console.ReadKey();
+                Console.Clear();
+
 
             }
         }
