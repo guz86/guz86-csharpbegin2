54ea17689c52914fb059d06ff69bc65b5fd84172
guz86 <guzmo6@rambler.ru>
2021-07-21 17:57:21 +0300

    tableReservation

---
+++ part2/tableReservation/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace tableReservation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // свободные места для бронирования
+            int[] tables = { 2, 4, 6, 5, 2, 3, 4, 2 };
+            bool openCafe = true;
+            while (openCafe)
+            {
+                Console.Write("1 - выберите столик, 2 - выход: ");
+
+                switch (Convert.ToInt32(Console.ReadLine()))
+                {
+                    case 1:
+
+                        break;
+                    case 2:
+                        openCafe = false;
+                        break;
+
+                }
+
+            }
+        }
+    }
+}
