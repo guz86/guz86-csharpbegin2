a24d2c9f1595159c4287aa4ea6ccf135695eec83
guz86 <guzmo6@rambler.ru>
2021-07-22 22:21:42 +0300

    ver1

---
+++ part2/sumOfEvenNumbersInTheArray/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace sumOfEvenNumbersInTheArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // найти сумму четных чисел в массиве
+
+            int[] myArray = { 1, 2, 3,4,5,6,7,8,9, 10 };
+            int evenSum = 0;
+            for (int i = 0; i < myArray.Length; i++)
+            {
+                 
+                if (myArray[i] % 2 == 0)
+                {
+                    evenSum += myArray[i];
+                }
+                
+            }
+            Console.WriteLine(evenSum);
+
+
+        }
+    }
+}
