509c1078adae1f0dfeb36c15c0fe296fc21139ac
guz86 <guzmo6@rambler.ru>
2021-07-22 22:43:19 +0300

    ver1

---
+++ part2/findTheSmallestNumberInTheArray/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace findTheSmallestNumberInTheArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] myArray = { 1, 2, 3, 4, 5, 6, -5, -6, -999 };
+            int smallValue = int.MaxValue;
+            for (int i = 0; i < myArray.Length; i++)
+            {
+                if (myArray[i] < smallValue)
+                {
+                    smallValue = myArray[i];
+                }
+                
+            }
+            Console.WriteLine(smallValue);
+        }
+    }
+}
