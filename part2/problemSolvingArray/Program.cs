f8979bae008026a4401a3e17a0b8f2848465714c
guz86 <guzmo6@rambler.ru>
2021-07-26 17:26:34 +0300

    ver1 find the two largest elements of the array

--- part2/problemSolvingArray/Program.cs
+++ part2/problemSolvingArray/Program.cs
@@ -6,7 +6,34 @@ namespace problemSolvingArray
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // найти два наибольших элемента массива
+
+            int[] myArray = { 23, 4, 78, 81,2,3,4,5,66,777,5 };
+            int maxElementOne= myArray[0];
+            int maxElementTwo = myArray[0];
+            int previous = myArray[0];
+            for (int i = 0; i < myArray.Length; i++)
+            {
+                if (myArray[i] > maxElementOne)
+                {
+                    previous = maxElementOne;
+                    maxElementOne = myArray[i];
+                }
+
+                if (myArray[i] > maxElementTwo)
+                {
+                    maxElementTwo = myArray[i];
+                }
+
+                if (maxElementOne == maxElementTwo)
+                {
+                    maxElementTwo = previous;
+                }
+            }
+
+            Console.WriteLine(maxElementOne + " "+maxElementTwo);
+
+
         }
     }
 }
