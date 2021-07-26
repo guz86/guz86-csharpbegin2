f4b55453bb120cc119c749b84b4e1a99e0114ff4
guz86 <guzmo6@rambler.ru>
2021-07-26 18:02:13 +0300

    task1 task2

--- part2/problemSolvingArray/Program.cs
+++ part2/problemSolvingArray/Program.cs
@@ -6,12 +6,17 @@ namespace problemSolvingArray
     {
         static void Main(string[] args)
         {
+            Task2();
+        }
+
+        public static void Task1()
+        {
             // найти два наибольших элемента массива
 
-            int[] myArray = { 23, 4, 78, 81,2,3,4,5,66,777,5 };
-            int maxElementOne= myArray[0];
-            int maxElementTwo = myArray[0];
-            int previous = myArray[0];
+            int[] myArray = { 283383, 33334, 78, 81, 2, 3, 4, 5, 66, 777, 885 };
+            int maxElementOne = int.MinValue;
+            int maxElementTwo = int.MinValue;
+            int previous = int.MinValue;
             for (int i = 0; i < myArray.Length; i++)
             {
                 if (myArray[i] > maxElementOne)
@@ -31,9 +36,23 @@ namespace problemSolvingArray
                 }
             }
 
-            Console.WriteLine(maxElementOne + " "+maxElementTwo);
+            Console.WriteLine(maxElementOne + " " + maxElementTwo);
 
 
         }
+        public static void Task2()
+        {
+            // найти два наибольших элемента массива
+
+
+
+
+
+        }
+
+
+
+
+
     }
 }
