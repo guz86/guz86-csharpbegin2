f0396d25b86cc4cfa00d4422360964af69f11ced
guz86 <guzmo6@rambler.ru>
2021-08-09 16:13:19 +0300

    init

---
+++ part2/TypeReductionAndTransformation/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace TypeReductionAndTransformation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // приведение и преобразование типов
+
+
+        }
+    }
+}
